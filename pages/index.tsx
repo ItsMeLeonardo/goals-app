207cd338282ba788f9db9c7d6db4723a86e5bec7
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-05-07 13:49:11 -0500

    FEAT: add dynamic stories

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,74 @@
 import type { NextPage } from "next";
 
+const data = [
+  {
+    profile:
+      "https://images.unsplash.com/photo-1520635360276-79f3dbd809f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2lybCUyMHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    username: "Anna Abel",
+    project: "Project 1",
+    project_poster:
+      "https://images.unsplash.com/photo-1583407723467-9b2d22504831?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cG9zdGVyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    profile:
+      "https://images.unsplash.com/photo-1474978528675-4a50a4508dc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Z2lybCUyMHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    username: "Gerogina Abel",
+    project: "Project 2",
+    project_poster:
+      "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    username: "Antonella Dabner",
+    profile:
+      "https://images.unsplash.com/photo-1505033575518-a36ea2ef75ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Z2lybCUyMHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    project: "Project 3",
+    project_poster:
+      "https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    username: "Valeria Dabner",
+    profile:
+      "https://images.unsplash.com/photo-1515202913167-d9a698095ebf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGdpcmwlMjBwcm9maWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    project: "Project 4",
+    project_poster:
+      "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    username: "Juan Dabner",
+    profile:
+      "https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    project: "Project 5",
+    project_poster:
+      "https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHRlY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    username: "Antony Dabner",
+    profile:
+      "https://images.unsplash.com/photo-1618641986557-1ecd230959aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    project: "Project 6",
+    project_poster:
+      "https://images.unsplash.com/photo-1539683255143-73a6b838b106?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHRlY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+];
+
 const Home: NextPage = () => {
   return (
     <>
-      <h3 className="subtitle">Messages</h3>
+      <h3 className="subtitle">Post recientes ({data.length})</h3>
       <ul className="story-list">
-        <li className="story">
-          <picture className="profile-photo">
-            <img src="https://images.unsplash.com/photo-1651868722945-fab942eaaec5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" />
-          </picture>
-          <p className="story-name">Your history</p>
-        </li>
-        <li className="story">
-          <picture className="profile-photo">
-            <img src="https://images.unsplash.com/photo-1651868722945-fab942eaaec5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" />
-          </picture>
-          <p className="story-name">Lilia james</p>
-        </li>
-        <li className="story">
-          <picture className="profile-photo">
-            <img src="https://images.unsplash.com/photo-1651868722945-fab942eaaec5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" />
-          </picture>
-          <p className="story-name">John Mark</p>
-        </li>
-        <li className="story">
-          <picture className="profile-photo">
-            <img src="https://images.unsplash.com/photo-1651868722945-fab942eaaec5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" />
-          </picture>
-          <p className="story-name">Anna Becker</p>
-        </li>
+        {data.map((story) => (
+          <li
+            key={story.username}
+            className="story"
+            //ts-ignore-next-line
+            style={{ backgroundImage: `url(${story.project_poster})` }}
+          >
+            <picture className="profile-photo">
+              <img src={story.profile} />
+            </picture>
+            <p className="story-name">{story.username}</p>
+          </li>
+        ))}
       </ul>
 
       <form className="create-post-form">
