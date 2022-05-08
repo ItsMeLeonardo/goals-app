dca76aaaa50dac18da3d659db120d3ccda3a0d74
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-05-08 09:53:27 -0500

    FEAT: complete login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,7 +1,71 @@
 export default function Login() {
   return (
     <>
-      <h1>Login wtf :v</h1>
+      <section className="page_content">
+        <aside className="form_container">
+          <h1 className="form_title">
+            Entra y aprendamos
+            <span className="logo"> Juntos_</span>
+          </h1>
+
+          <button className="btn github_button">
+            <i className="uil uil-github-alt"></i>
+            <span>Github</span>
+          </button>
+          <button className="btn google_button">
+            <i className="uil uil-google"></i>
+            <span>Google</span>
+          </button>
+        </aside>
+      </section>
+
+      <style jsx>{`
+        .page_content {
+          width: 100%;
+          height: 50vh;
+          display: grid;
+          place-content: center;
+        }
+        .form_container {
+          background: var(--color-white);
+          height: 100%;
+          width: 100vw;
+          max-width: 280px;
+          border-radius: var(--card-radius);
+          display: flex;
+          gap: 1rem;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          padding: var(--card-padding);
+        }
+        .form_title {
+          font-size: 1.25rem;
+          text-align: center;
+        }
+        .logo {
+          color: var(--color-primary);
+        }
+        .google_button,
+        .github_button {
+          width: 100%;
+          display: flex;
+          gap: 0.5rem;
+          justify-content: center;
+          align-items: center;
+          color: var(--color-white);
+        }
+        .google_button i,
+        .github_button i {
+          font-size: 1rem;
+        }
+        .google_button {
+          background: var(--color-primary);
+        }
+        .github_button {
+          background: var(--color-black);
+        }
+      `}</style>
     </>
   );
 }
