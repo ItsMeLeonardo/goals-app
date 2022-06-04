db80d0b791ad5beb7c01207a114c80cecb46a541
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-06-04 16:59:11 -0500

    FEAT: add error styles

--- components/Form/FormInput.tsx
+++ components/Form/FormInput.tsx
@@ -1,16 +1,38 @@
-import { DetailedHTMLProps, InputHTMLAttributes, ReactNode } from "react";
+import {
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  ReactNode,
+  useEffect,
+  useState,
+} from "react";
 
 import Form from "./form.module.css";
 
 type props = {
   icon?: ReactNode;
+  error?: string;
 } & DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
-export default function FormInput({ icon, ...props }: props) {
+export default function FormInput({ icon, error, ...props }: props) {
+  const [hasError, setHasError] = useState<string | null>(error || null);
+
+  useEffect(() => {
+    let timeOut: NodeJS.Timeout;
+    if (error) {
+      timeOut = setTimeout(() => setHasError(null), 5000);
+    }
+
+    return () => clearTimeout(timeOut);
+  }, [error]);
+
   return (
     <label className={Form.field}>
       <span className={Form.field_icon}>{icon}</span>
-      <input {...props} className={Form.input} />
+      <input
+        {...props}
+        className={`${Form.input} ${hasError && Form.input_error}`}
+      />
+      {hasError && <span className={Form.error_message}>{error}</span>}
     </label>
   );
 }
