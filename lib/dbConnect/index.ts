e27979884b1faa4cf2282b0651300082faa29b62
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-06-12 14:31:31 -0500

    FIX: console.log

--- lib/dbConnect/index.ts
+++ lib/dbConnect/index.ts
@@ -41,7 +41,6 @@ async function dbConnect() {
     };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      console.log({ mongoose });
       return mongoose;
     });
   }
