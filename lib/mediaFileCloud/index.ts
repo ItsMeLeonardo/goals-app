fccc981fd549a8615f0d31578a36643bb3512290
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-06-20 14:41:51 -0500

    FEAT: add integration with cloudinary

---
+++ lib/mediaFileCloud/index.ts
@@ -0,0 +1,48 @@
+import {
+  v2 as cloudinary,
+  UploadApiResponse,
+  UploadApiErrorResponse,
+  UploadApiOptions,
+  UploadResponseCallback,
+} from "cloudinary";
+
+export type mediaType = {
+  url: string;
+  name: string;
+};
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+function uploadStream(
+  buffer: Buffer,
+  config: UploadApiOptions
+): Promise<UploadApiResponse | UploadApiErrorResponse> {
+  return new Promise((resolve, reject) => {
+    const cloudinaryDone: UploadResponseCallback = (error, result) => {
+      console.log({ result });
+      return !error && result ? resolve(result) : reject(error);
+    };
+    cloudinary.uploader.upload_stream(config, cloudinaryDone).end(buffer);
+  });
+}
+
+export async function uploadImage(
+  file: Express.Multer.File
+): Promise<[mediaType | null, string | null]> {
+  try {
+    const imageOptions = { folder: process.env.CLOUDINARY_FOLDER };
+    const { public_id: name, secure_url: url } = await uploadStream(
+      file.buffer,
+      imageOptions
+    ).catch();
+
+    return [{ url, name }, null];
+  } catch (error) {
+    console.error({ error });
+    return [null, "Error cargando la imagen"];
+  }
+}
