db06c46bfb5dcbed1a75f823dd2d0fec43e6574d
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-06-20 16:21:07 -0500

    FEAT: add swr config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import "../styles/globals.css";
+import { SWRConfig, SWRConfiguration } from "swr";
+
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 
@@ -19,21 +20,31 @@ type Props = {
   Component: RequireAuthComponent;
 } & AppProps;
 
+//styles
+import "../styles/globals.css";
+
+//swr config
+const config: SWRConfiguration = {
+  revalidateOnFocus: false,
+};
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }: Props) {
   return (
-    <SessionProvider session={session}>
-      {Component.requireAuth ? (
-        <RequireAuth>
-          <UserLayout>
+    <SWRConfig value={config}>
+      <SessionProvider session={session}>
+        {Component.requireAuth ? (
+          <RequireAuth>
+            <UserLayout>
+              <Component {...pageProps} />
+            </UserLayout>
+          </RequireAuth>
+        ) : (
+          <MainLayout>
             <Component {...pageProps} />
-          </UserLayout>
-        </RequireAuth>
-      ) : (
-        <MainLayout>
-          <Component {...pageProps} />
-        </MainLayout>
-      )}
-    </SessionProvider>
+          </MainLayout>
+        )}
+      </SessionProvider>
+    </SWRConfig>
   );
 }
 
