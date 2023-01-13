abe1fdc9c49dd69186b78cac25ac50365508a079
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-01-13 18:26:34 -0500

    add loader to explore search bat

--- components/Explore/index.tsx
+++ components/Explore/index.tsx
@@ -1,84 +1,88 @@
-import { debounce } from "throttle-debounce";
-import { ChangeEvent, useState } from "react";
+import { debounce } from 'throttle-debounce'
+import { ChangeEvent, useState } from 'react'
 
-import PostList from "components/Post/PostList";
+import PostList from 'components/Post/PostList'
 
-import { search } from "services/post";
+import { search } from 'services/post'
 
-import { Post } from "models/post";
+import { Post } from 'models/post'
 
-import styles from "./Explore.module.css";
+import styles from './Explore.module.css'
 
 const categories = [
-  {
-    name: "Programación",
-    value: "programing",
-  },
-  {
-    name: "Diseño",
-    value: "design",
-  },
-  {
-    name: "Música",
-    value: "music",
-  },
-  {
-    name: "Cine",
-    value: "cine",
-  },
-];
+	{
+		name: 'Programación',
+		value: 'programing',
+	},
+	{
+		name: 'Diseño',
+		value: 'design',
+	},
+	{
+		name: 'Música',
+		value: 'music',
+	},
+	{
+		name: 'Cine',
+		value: 'cine',
+	},
+]
 
 export default function Explore() {
-  const [postResult, setPostResult] = useState<Post[]>([]);
-  const [query, setQuery] = useState<string | null>(null);
+	const [postResult, setPostResult] = useState<Post[]>([])
+	const [loading, setLoading] = useState(false)
+	const [query, setQuery] = useState<string | null>(null)
 
-  const searchPost = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    if (value.trim().length < 2) return;
-    search(value).then((posts) => {
-      setPostResult(posts);
-      setQuery(value);
-    });
-  };
+	const searchPost = (event: ChangeEvent<HTMLInputElement>) => {
+		const { value } = event.target
+		if (value.trim().length < 2) return
+		setLoading(true)
+		search(value)
+			.then((posts) => {
+				setPostResult(posts)
+				setQuery(value)
+			})
+			.finally(() => setLoading(false))
+	}
 
-  const debouncedSearchPost = debounce(500, searchPost);
+	const debouncedSearchPost = debounce(500, searchPost)
 
-  return (
-    <>
-      <h3>Explore here</h3>
-      <form className={styles.form}>
-        <input
-          type="search"
-          name=""
-          id=""
-          className={styles.search}
-          placeholder="¿Qué quieres aprender hoy?"
-          onChange={debouncedSearchPost}
-        />
-        <ul className={styles.category_list}>
-          {categories.map(({ value, name }) => (
-            <li key={value}>
-              <input
-                className={styles.category_input}
-                type="checkbox"
-                id={value}
-                hidden
-              />
-              <label htmlFor={value} className={styles.category}>
-                {name}
-              </label>
-            </li>
-          ))}
-        </ul>
-      </form>
+	return (
+		<>
+			<label htmlFor="explore">
+				<h3>Explore here</h3>
+			</label>
+			<div className={styles.form}>
+				<div className={styles.inputContainer}>
+					<input
+						type="search"
+						name=""
+						id="explore"
+						className={styles.search}
+						placeholder="¿Qué quieres aprender hoy?"
+						onChange={debouncedSearchPost}
+					/>
+					{loading && <div className={styles.loader} />}
+				</div>
+				<ul className={styles.category_list}>
+					{categories.map(({ value, name }) => (
+						<li key={value}>
+							<input className={styles.category_input} type="checkbox" id={value} hidden />
+							<label htmlFor={value} className={styles.category}>
+								{name}
+							</label>
+						</li>
+					))}
+				</ul>
+			</div>
 
-      {query && (
-        <h4 className="subtitle-sm">
-          Resultados para <span className="text-primary">{query}</span>
-        </h4>
-      )}
+			{query && (
+				<h4 className="subtitle-sm">
+					Resultados para <span className="text-primary">{query}</span>
+				</h4>
+			)}
 
-      <PostList posts={postResult} />
-    </>
-  );
+			<PostList posts={postResult} />
+		</>
+	)
 }
