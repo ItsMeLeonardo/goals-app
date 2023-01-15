fa7ac887b808da8fa31ae369c1d374437db58c6d
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-01-15 11:39:16 -0500

    fix loading for autocomplete

--- components/Pages/Share/AutocompleteTag/AutocompleteResults.tsx
+++ components/Pages/Share/AutocompleteTag/AutocompleteResults.tsx
@@ -1,68 +1,55 @@
-import style from "./autocomplete.module.css";
+import style from './autocomplete.module.css'
 
-import { create } from "services/tags";
+import { create } from 'services/tags'
 
 import type {
-  AutocompleteResultsProps,
-  AutocompleteItem,
-} from "components/Pages/Share/AutocompleteTag/types";
+	AutocompleteResultsProps,
+	AutocompleteItem,
+} from 'components/Pages/Share/AutocompleteTag/types'
+import Loader from 'components/shared/Loader'
 
 export default function AutocompleteResults(props: AutocompleteResultsProps) {
-  const { onSelect, results, loading, querySearch } = props;
+	const { onSelect, results, loading, querySearch } = props
 
-  const addTag = async () => {
-    const newTag = await create(querySearch);
-    if (!newTag || !newTag.data) return;
-    const { name, id } = newTag.data;
+	const addTag = async () => {
+		const newTag = await create(querySearch)
+		if (!newTag || !newTag.data) return
+		const { name, id } = newTag.data
 
-    const tag: AutocompleteItem = {
-      id: id.toString(),
-      name,
-      isSelected: true,
-    };
-    onSelect(tag);
-  };
+		const tag: AutocompleteItem = {
+			id: id.toString(),
+			name,
+			isSelected: true,
+		}
+		onSelect(tag)
+	}
 
-  return (
-    <ul className={style.list}>
-      {results.map((item) => (
-        <li
-          className={style.item}
-          key={item.id}
-          role="button"
-          onClick={() => onSelect(item)}
-        >
-          <span>{item.name}</span>
-          <span
-            className={`${style.icon_check} ${
-              item.isSelected && style.icon_check_active
-            }`}
-          >
-            <i className="uil uil-check"></i>
-          </span>
-        </li>
-      ))}
+	return (
+		<ul className={style.list}>
+			{results.map((item) => (
+				<button className={style.item} key={item.id} role="button" onClick={() => onSelect(item)}>
+					<span>{item.name}</span>
+					<span className={`${style.icon_check} ${item.isSelected && style.icon_check_active}`}>
+						<i className="uil uil-check"></i>
+					</span>
+				</button>
+			))}
 
-      {results.length === 0 && !loading && (
-        <li className={style.item} onClick={addTag} role="button">
-          <p>
-            Presiona
-            <strong> Enter </strong>o has
-            <strong> Click </strong>
-            para agregar
-            <span className="font-i">{` ${querySearch}`}</span>
-          </p>
-        </li>
-      )}
+			{results.length === 0 && !loading && (
+				<button className={style.item} onClick={addTag} role="button">
+					<p>
+						<strong> Click </strong>
+						para agregar <strong className="font-i">{querySearch}</strong>
+					</p>
+				</button>
+			)}
 
-      {loading && (
-        <li className={style.item}>
-          <span>Cargando ...</span>
-          <span className={style.icon_loading}>
-            <i className="uil uil-spinner"></i>
-          </span>
-        </li>
-      )}
-    </ul>
-  );
+			{loading && (
+				<li className={style.item}>
+					<span>Buscando</span>
+					<Loader size={20} />
+				</li>
+			)}
+		</ul>
+	)
 }
